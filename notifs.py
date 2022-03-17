2c6b3673ffd64246fc12762f97de69c07c41aab4
HugoFerry <72927744+HugoFerry@users.noreply.github.com>
2022-03-17 22:42:05 +0100

    Create notifs.py

---
+++ notifs.py
@@ -0,0 +1,8 @@
+from plyer import notification
+
+notification.notify(
+    title = 'Sauvegarde',
+    message = "Votre sauvegarde vient de s'effectuer ! Passez une agréable journée !",
+    app_icon = "savecorp.ico",
+    timeout = 10,
+)
